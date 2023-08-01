f140b07ff4988d6f75b15c90e6699c4094346403
Sarthak Sharma <sarthaksharma0807@gmail.com>
2023-08-01 17:33:34 +0530

    trying to fix CORS

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ connectToMongo();
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+    origin:'https://e-commerce-ashen-five.vercel.app'
+}))
 app.use(express.json())
 
 app.get('/',(req,res)=>{
